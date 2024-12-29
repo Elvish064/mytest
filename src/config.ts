d8b8ed8dc3c2c59e2c75808704506dae29e30fb4
Elvish064 <3356832185@qq.com>
2024-12-29 17:45:26 +0800

    fix: 移除导航栏配置中的友链部分

--- src/config.ts
+++ src/config.ts
@@ -42,7 +42,6 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    LinkPreset.Friends,
     {
       name: 'GitHub',
       url: 'https://github.com/Elvish064',     // Internal links should not include the base path, as it is automatically added
